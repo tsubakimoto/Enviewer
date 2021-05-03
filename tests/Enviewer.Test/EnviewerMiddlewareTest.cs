89ebe8c302497328f9ff6c4725530fc6dd0cc959
tkmbmbr <tkmbmbr@gmail.com>
2021-05-03 11:43:04 +0900

    Add unit test

---
+++ tests/Enviewer.Test/EnviewerMiddlewareTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Enviewer.Test
+{
+    public class EnviewerMiddlewareTest
+    {
+        [Fact]
+        async Task ReturnsOkForRequest()
+        {
+            using var host = await new HostBuilder()
+                .ConfigureWebHost(builder =>
+                {
+                    builder
+                        .UseTestServer()
+                        .ConfigureServices(services =>
+                        {
+
+                        })
+                        .Configure(app =>
+                        {
+                            app.UseEnviewer();
+                        });
+                })
+                .StartAsync();
+
+            var response = await host.GetTestClient().GetAsync("/enviewer");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}

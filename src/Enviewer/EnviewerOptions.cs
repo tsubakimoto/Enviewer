27c7039951d360e1bf84f979ed3d1ce01040694d
tkmbmbr <tkmbmbr@gmail.com>
2021-05-04 23:07:41 +0900

    Add middleware options

---
+++ src/Enviewer/EnviewerOptions.cs
@@ -0,0 +1,21 @@
+﻿namespace Enviewer
+{
+    public class EnviewerOptions
+    {
+        private static readonly string DEFAULT_ROUTE = "/enviewer";
+
+        public EnviewerOptions()
+        {
+        }
+
+        /// <summary>
+        /// Sets a custom route for the Enviewer endpoint(s).
+        /// </summary>
+        public string Route
+        {
+            get { return route; }
+            set { route = string.IsNullOrWhiteSpace(value) ? DEFAULT_ROUTE : value; }
+        }
+        private string route = DEFAULT_ROUTE;
+    }
+}

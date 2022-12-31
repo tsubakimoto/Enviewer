2e6bc7347ff43cd7ff8174bea1079e689875d048
Yuta Matsumura <tkmbmbr@gmail.com>
2022-12-31 09:31:16 +0900

    #3 Add unit tests

---
+++ tests/Enviewer.Test/EnviewerOptionsTest.cs
@@ -0,0 +1,26 @@
+﻿namespace Enviewer.Test;
+
+public class EnviewerOptionsTest
+{
+    [Fact]
+    void SuccessForConstructor()
+    {
+        EnviewerOptions options = new ();
+        Assert.Equal("/enviewer", options.Route);
+    }
+
+    [Theory]
+    [InlineData(null, "/enviewer")]
+    [InlineData("", "/enviewer")]
+    [InlineData(" ", "/enviewer")]
+    [InlineData("/", "/")]
+    [InlineData("/hoge", "/hoge")]
+    void SuccessForRouteSetter(string route, string expected)
+    {
+        EnviewerOptions options = new()
+        {
+            Route = route
+        };
+        Assert.Equal(expected, options.Route);
+    }
+}

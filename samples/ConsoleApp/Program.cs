e79c0650cf03ef0d6080976cfaf677e73d9a8d10
Yuta Matsumura <tkmbmbr@gmail.com>
2026-02-23 23:32:41 +0900

    Revert "add console sample"
    This reverts commit 320e94224ff89b17341812b901235605797dd31d.

--- samples/ConsoleApp/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
-HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<ExampleHostedService>();
-
-using IHost host = builder.Build();
-await host.RunAsync();

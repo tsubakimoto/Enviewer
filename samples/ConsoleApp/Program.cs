320e94224ff89b17341812b901235605797dd31d
Yuta Matsumura <tkmbmbr@gmail.com>
2026-02-23 23:15:34 +0900

    add console sample

---
+++ samples/ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<ExampleHostedService>();
+
+using IHost host = builder.Build();
+await host.RunAsync();
